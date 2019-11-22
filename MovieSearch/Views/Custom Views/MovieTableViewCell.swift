49eb19361e89b04c556af832b4a7c93c1b4db1e4
Aaron Shackelford <burntcreamlatte@icloud.com>
2019-11-22 11:07:01 -0700

    beginning work on custom cell

--- MovieSearch/Views/Custom Views/MovieTableViewCell.swift
+++ MovieSearch/Views/Custom Views/MovieTableViewCell.swift
@@ -15,17 +15,7 @@ class MovieTableViewCell: UITableViewCell {
             updateViews()
         }
     }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
     func updateViews() {
         
     }
